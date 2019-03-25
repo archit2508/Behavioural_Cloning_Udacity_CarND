b32a193409356aa53b253af78bde819f6c13528d
archit69 <architgupta690@gmail.com>
2019-03-25 04:34:57 +0000

    comments added for model training code

--- model.py
+++ model.py
@@ -10,30 +10,17 @@ from keras.layers import Lambda, Conv2D, Cropping2D
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
-lines = []
+lines = []  #Array which will store all data items in csv file
 with open('/opt/carnd_p3/data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)
     for line in reader :
         lines.append(line)
-        
-# images = []
-# measurements = []
 
-# for line in lines:
-#     source_path = line[0]
-#     filename = source_path.split('/')[-1]
-#     current_path = '/opt/carnd_p3/data/IMG/' + filename
-#     image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB)
-#     images.append(image)
-#     measurement = float(line[3])
-#     measurements.append(measurement)
-    
-# X_train = np.array(images)
-# y_train = np.array(measurements)
-
+#85% data will be used for training and 15% for validation
 training_data, validation_data = train_test_split(lines,test_size=0.15)
 
+#Generator which will return data items in batches of 32 items
 def generator(lines, batch_size=32):
     len_data = len(lines)
     while 1: 
@@ -43,28 +30,11 @@ def generator(lines, batch_size=32):
             images = []
             angles = []
             for line in batch_data:
-#                     for i in range(0,3):
-                        path = '/opt/carnd_p3/data/IMG/'+line[0].split('/')[-1]
-                        center_image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB) 
-                        center_angle = float(line[3])
-                        images.append(center_image)
-                        angles.append(center_angle)
-                                                
-#                         if(i==0):
-#                             angles.append(center_angle)
-#                         elif(i==1):
-#                             angles.append(center_angle+0.2)
-#                         elif(i==2):
-#                             angles.append(center_angle-0.2)
-                        
-#                         images.append(cv2.flip(center_image,1))
-#                         if(i==0):
-#                             angles.append(center_angle*-1)
-#                         elif(i==1):
-#                             angles.append((center_angle+0.2)*-1)
-#                         elif(i==2):
-#                             angles.append((center_angle-0.2)*-1)                        
-        
+                path = '/opt/carnd_p3/data/IMG/'+line[0].split('/')[-1]
+                center_image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB) 
+                center_angle = float(line[3])
+                images.append(center_image)
+                angles.append(center_angle)            
             X_train = np.array(images)
             y_train = np.array(angles)
             
@@ -73,24 +43,36 @@ def generator(lines, batch_size=32):
 train_generator = generator(training_data, batch_size=32)
 validation_generator = generator(validation_data, batch_size=32)
 
+#NVIDIA Model architecture used to train the model
 model = Sequential()
+#Preprocessing incoming data using normalization and generalizing small standard deviation for all data
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+#Cropping the images in dataset to remove sky and trees section to have only area containing road for better training
 model.add(Cropping2D(cropping=((70,25),(0,0))))
+#layer 1- Convolution, no of filters- 24, filter size= 5x5, stride= 2x2
 model.add(Conv2D(24, (5, 5), strides =(2,2), activation="relu"))
+#layer 2- Convolution, no of filters- 36, filter size= 5x5, stride= 2x2
 model.add(Conv2D(36, (5, 5), strides =(2,2), activation="relu"))
+#layer 3- Convolution, no of filters- 48, filter size= 5x5, stride= 2x2
 model.add(Conv2D(48, (5, 5), strides =(2,2), activation="relu"))
+#layer 4- Convolution, no of filters- 64, filter size= 3x3, stride= 1x1
 model.add(Conv2D(64, (3, 3), activation="relu"))
+#layer 5- Convolution, no of filters- 64, filter size= 3x3, stride= 1x1
 model.add(Conv2D(64, (3, 3), activation="relu"))
+#flattening image shape into a single array 
 model.add(Flatten())
+#layer 6- fully connected layer
 model.add(Dense(100))
-model.add(Dense(50))
+#layer 7- fully connected layer 1
+model.add(Dense(50)) 
+#layer 8- fully connected layer 1
 model.add(Dense(10))
+#layer 9- fully connected layer 1
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, validation_steps=len(validation_data), epochs=1, 
                     validation_data=validation_generator, steps_per_epoch= len(training_data))
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
     
